05971502f87733886a59e7b177fc077765745572
endpointclients <endpointclients@gmail.com>
2026-01-26 12:26:53 +0500

    Theme Added.

---
+++ components/headers/Header1.tsx
@@ -0,0 +1,61 @@
+"use client";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import AnimatedButton from "../animation/AnimatedButton";
+import { usePathname } from "next/navigation";
+import ThemeSwitcherButton from "./ColorSwitcher";
+import Image from "next/image";
+
+export default function Header1() {
+  const pathname = usePathname();
+  const [isHidden, setIsHidden] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      setIsHidden(currentScrollPos > 10);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <header id="header" className={`mxd-header ${isHidden ? "is-hidden" : ""}`}>
+      {/* header logo */}
+      <div className="mxd-header__logo loading__fade ">
+        <Link href={`/home-main`} className="">
+          <Image
+            alt="logo epc"
+            src={"img/logoHorizontal.svg"}
+            width={40}
+            height={40}
+            className="h-24! "
+          />
+        </Link>
+      </div>
+      {/* header controls */}
+      {/* <div className="mxd-header__controls loading__fade">
+        <ThemeSwitcherButton />
+
+        {pathname == "/" || pathname == "/preview" ? (
+          <AnimatedButton
+            text="Purchase"
+            className="btn btn-anim btn-default btn-mobile-icon btn-outline slide-right"
+            href="https://themeforest.net/user/ib-themes/portfolio"
+            target="_blank"
+          >
+            <i className="ph-bold ph-shopping-cart-simple" />
+          </AnimatedButton>
+        ) : (
+          <AnimatedButton
+            text="Say Hello"
+            className="btn btn-anim btn-default btn-mobile-icon btn-outline slide-right"
+            href="/contact"
+          >
+            <i className="ph-bold ph-arrow-up-right" />
+          </AnimatedButton>
+        )}
+      </div> */}
+    </header>
+  );
+}
